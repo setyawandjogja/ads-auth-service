aaeeebc8c784334ea8854d9bb2e67b18b28b396f
raptor <setiawan.df@gmail.com>
2026-02-28 19:11:47 +0700

    refactor

--- src/test/java/ads/user_management_service/UserManagementServiceApplicationTests.java
+++ src/test/java/ads/user_management_service/UserManagementServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AutserviceApplicationTests {
+class UserManagementServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
